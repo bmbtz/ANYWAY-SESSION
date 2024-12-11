b09dae860e7d6a5ac26f809b5ea6e62c10ccca27
mr Anyway <simonstephano328@gmail.com>
2024-12-11 10:13:46 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,26 +56,26 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id, { text: '' + b64data });
 
                let VENOCYBER_MD_TEXT = `
-*_Pair Code Connected by Venocyber Tech_*
+*_Pair Code Connected by 𝐀𝐍𝐘𝐖𝐀𝐘 𝐓𝐄𝐂𝐇_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN VENOCYBER-MD 』*
+║ *『 WOW YOU CHOOSEN 𝐀𝐍𝐘𝐖𝐀𝐘-𝐌𝐃 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚══════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@JASTINMTEWA-vn9pl_
-║❒ *Owner:* _https://wa.me/message/A4QG2JZKBXFTN1_
-║❒ *Repo:* _https://github.com/Kingjux/venocyber-md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/HSln3blDuuuKvC8njxyCCN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l_
+║❒ *Ytube:* _https://youtube.com/@anyway-tech?si=12V7rOPscbntLzU4_
+║❒ *Owner:* _t.me/Anywaytech_
+║❒ *Repo:* _https://github.com/anywaytech2/ANYWAY-MD-V1_
+║❒ *WaGroup:* _https://chat.whatsapp.com/JpaDLilaPx31vSbaUp1XbS_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VagWQ255q08VTCRQKP09_
 ║❒ *Plugins:* _https://github.com/Kingjux/venocyber-md-plugins_
 ╚══════════════════════╝ 
 _____________________________________
 
 _Don't Forget To Give Star To My Repo_`
- await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id,{text:VENOCYBER_MD_TEXT},{quoted:session})
+ await Pair_Code_By_anyway_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id,{text:VENOCYBER_MD_TEXT},{quoted:session})
  
 
         await delay(100);
