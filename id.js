af8f50154b18a65bd1922499285786533bf94807
mr Anyway <simonstephano328@gmail.com>
2025-09-25 21:47:00 +0300

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,9 @@
-function makeid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+function makeid(length = 10) {
+  let result = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return result;
 }
-module.exports = {makeid};
+module.exports = { makeid };
